82f92b244767e9ce80dd828b5584e4951349c441
eVW3 <1happyrock1@gmail.com>
2021-07-12 10:24:58 +0300

    init

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export const API_URL = `http://localhost:${process.env.PORT}`;
 export const REDIRECT_URI = `${API_URL}/auth/google-sign-up`;
 export const REDIRECT_TO_FRONT = () => {
-  return process.env.NODE_ENV === 'development' ? `http://localhost:3002` : `${API_URL}`
+  return process.env.NODE_ENV === 'development' ? `http://localhost:3002` : `http://localhost:3002`
 }
